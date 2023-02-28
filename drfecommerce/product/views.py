b3bc036b45aeeacd3cafc1c95132488decb4b5f2
Adamyuyue <67887367+Adamyuyue@users.noreply.github.com>
2023-02-27 20:56:14 -0600

    added Product Media and Testing

--- drfecommerce/product/views.py
+++ drfecommerce/product/views.py
@@ -1,4 +1,5 @@
 # from django.db import connection
+from django.db.models import Prefetch
 from drf_spectacular.utils import extend_schema
 
 # from pygments import highlight
@@ -50,7 +51,9 @@ class ProductViewSet(viewsets.ModelViewSet):
 
     def retrieve(self, request, slug=None):
         serializer = ProductSerializer(
-            self.queryset.filter(slug=slug).select_related("category", "brand"),
+            Product.objects.filter(slug=slug)
+            .select_related("category", "brand")
+            .prefetch_related(Prefetch("product_line__product_image")),
             many=True,
         )
         data = Response(serializer.data)
